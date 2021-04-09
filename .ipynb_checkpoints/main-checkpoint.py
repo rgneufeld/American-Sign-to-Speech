0902fa37de38a62b94b135d872c116c959378803
Rod Neufeld <rgneufeld@gmail.com>
2021-04-08 20:09:44 -0700

    Clean up code

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,25 +1,33 @@
-# Import all the Required modules
+from __future__ import print_function
 
-from PIL import Image, ImageTk
-import pyttsx3
-import tkinter as tk
-import cv2
-from keras.models import model_from_json
 import operator
+import tkinter as tk
 from string import ascii_uppercase
 
+import cv2
+import hunspell
+import pyttsx3
+from PIL import Image, ImageTk
+from keras.models import model_from_json
 
-# Create the main class of the application
+
 class Application:
     def __init__(self):
-
+        language = "en_US"
+        gender = "VoiceGenderFemale"
         # Initiate some of the properties of text to voice function
         self.engine = pyttsx3.init()
         self.engine.setProperty("rate", 200)  # Set the speed of the speaker
-        self.voices = self.engine.getProperty("voices")  # Get all the available voices
-        self.engine.setProperty("voice", self.voices[1].id)
+        for self.voice in self.engine.getProperty('voices'):
+            print(self.voice)
 
-        # To get the access to the camera and get frames
+        # self.voices = self.engine.getProperty("voices")  # Get all the available voices
+        for self.voice in self.engine.getProperty('voices'):
+            if language in self.voice.languages and gender == self.voice.gender:
+                self.engine.setProperty("voice", self.voice.id)
+        # self.engine.setProperty("voice", self.voices[1].id)
+
+        # self.hs = hunspell.HunSpell('/usr/share/hunspell/en_US.dic', '/usr/share/hunspell/en_US.aff')
         self.vs = cv2.VideoCapture(0)
         self.current_image = None
         self.current_image2 = None
@@ -27,33 +35,32 @@ class Application:
         # Here we create a variable to store the address of the models
         self.directory = 'model'
 
-        # Import all the required Neural Netwroks
-        self.json_file = open(self.directory + "\model-bw.json", "r")
+        # Import all the required Neural Networks
+        self.json_file = open(self.directory + "/model-bw.json", "r")
         self.model_json = self.json_file.read()
         self.json_file.close()
         self.loaded_model = model_from_json(self.model_json)
-        self.loaded_model.load_weights(self.directory + "\model-bw.h5")
+        self.loaded_model.load_weights(self.directory + "/model-bw.h5")
 
-        self.json_file_dru = open(self.directory + "\model-bw_dru.json", "r")
+        self.json_file_dru = open(self.directory + "/model-bw_dru.json", "r")
         self.model_json_dru = self.json_file_dru.read()
         self.json_file_dru.close()
         self.loaded_model_dru = model_from_json(self.model_json_dru)
-        self.loaded_model_dru.load_weights(self.directory + "\model-bw_dru.h5")
+        self.loaded_model_dru.load_weights(self.directory + "/model-bw_dru.h5")
 
-        self.json_file_tkdi = open(self.directory + "\model-bw_tkdi.json", "r")
+        self.json_file_tkdi = open(self.directory + "/model-bw_tkdi.json", "r")
         self.model_json_tkdi = self.json_file_tkdi.read()
         self.json_file_tkdi.close()
         self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
-        self.loaded_model_tkdi.load_weights(self.directory + "\model-bw_tkdi.h5")
+        self.loaded_model_tkdi.load_weights(self.directory + "/model-bw_tkdi.h5")
 
-        self.json_file_smn = open(self.directory + "\model-bw_smn.json", "r")
+        self.json_file_smn = open(self.directory + "/model-bw_smn.json", "r")
         self.model_json_smn = self.json_file_smn.read()
         self.json_file_smn.close()
         self.loaded_model_smn = model_from_json(self.model_json_smn)
-        self.loaded_model_smn.load_weights(self.directory + "\model-bw_smn.h5")
+        self.loaded_model_smn.load_weights(self.directory + "/model-bw_smn.h5")
 
-        self.ct = {}
-        self.ct['Blank'] = 0
+        self.ct = {'blank': 0}
         self.blank_flag = 0
 
         # Here "i" represent each character from A-Z. and set 0 to each unit of the ct array
@@ -61,7 +68,7 @@ class Application:
             self.ct[i] = 0
 
         self.root = tk.Tk()
-        self.root.title("American-Sign-to-Speech")
+        self.root.title("Sign language to Text Converter")
         self.root.protocol('WM_DELETE_WINDOW', self.destructor)
         self.root.geometry("800x800")  # Set the intital size of the window
 
@@ -76,13 +83,33 @@ class Application:
         self.T.config(text="American-Sign-to-Speech", font=("courier", 40, "bold"))
 
         self.panel3 = tk.Label(self.root)  # set the location of the predictive symmol/ Character value
-        self.panel3.place(x=450, y=610)
+        self.panel3.place(x=1200, y=50)
 
         self.T1 = tk.Label(self.root)  # set the location of the character TEXT in the window
-        self.T1.place(x=30, y=610)
+        self.T1.place(x=780, y=50)
         self.T1.config(text="Character :", font=("Courier", 40, "bold"))
 
-        # Create Following variables and initialize them as empty variables
+        self.panel4 = tk.Label(self.root)  # word vaule
+        self.panel4.place(x=1200, y=100)
+
+        self.T2 = tk.Label(self.root)  # "Word" represent on the interface
+        self.T2.place(x=780, y=100)
+        self.T2.config(text="Word :", font=("Courier", 40, "bold"))
+
+        self.panel5 = tk.Label(self.root)  # Sentence value
+        self.panel5.place(x=1200, y=150)
+
+        self.T3 = tk.Label(self.root)  # "Sentence" represent on teh interface
+        self.T3.place(x=780, y=150)
+        self.T3.config(text="Sentence :", font=("Courier", 40, "bold"))
+
+        self.T4 = tk.Label(self.root)  # Suggestions
+        self.T4.place(x=780, y=200)
+        self.T4.config(text="Suggestions", fg="red", font=("Courier", 40, "bold"))
+
+        # Here are some variables,
+        self.str = ""
+        self.word = ""
         self.current_symbol = "Empty"
         self.photo = "Empty"
 
@@ -130,22 +157,24 @@ class Application:
             th3 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
             ret, res = cv2.threshold(th3, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-            # Pass it to predict the symbol of the current frame of the camera
+            # Pass it to pridict the symbol of the currrent frame of the camera
             self.predict(res)
             self.current_image2 = Image.fromarray(res)
 
-            # Now pass the current symbol, predictive word and string will be pass to our interface
+            # Now pass the curent symbol, predictive word and string will be pass to our interface
             # TO show the results
             imgtk = ImageTk.PhotoImage(image=self.current_image2)
             self.panel2.imgtk = imgtk
             self.panel2.config(image=imgtk)
             self.panel3.config(text=self.current_symbol, font=("Courier", 50))  # Current Symbol
+            self.panel4.config(text=self.word, font=("Courier", 40))  # Word Predicted
+            self.panel5.config(text=self.str, font=("Courier", 40))  # Whole Sentence
 
         self.root.after(30, self.video_loop)
 
     def predict(self, test_image):
 
-        # Resize the image using "resize" function of the cv2
+        # Resize the iamge using "resize" function of the cv2
         test_image = cv2.resize(test_image, (128, 128))
 
         # MAIN Layer to predict from A-Z. Its the main predictive layer
@@ -155,53 +184,38 @@ class Application:
         result_tkdi = self.loaded_model_tkdi.predict(test_image.reshape(1, 128, 128, 1))
         result_smn = self.loaded_model_smn.predict(test_image.reshape(1, 128, 128, 1))
 
-        prediction = {}
-        prediction['Blank'] = result[0][0]
+        prediction = {'blank': result[0][0]}
         inde = 1
         for i in ascii_uppercase:
             prediction[i] = result[0][inde]
             inde += 1
 
-        # LAYER 1: IT covers all characters from A-Z
+        # LAYER 1
         prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
         self.current_symbol = prediction[0][0]
 
-        # LAYER 2 start from here
-
-        # For a group of character, which includes D, R, and U.
-        if (self.current_symbol == 'D' or self.current_symbol == 'R' or self.current_symbol == 'U'):
-            prediction = {}
-            prediction['D'] = result_dru[0][0]
-            prediction['R'] = result_dru[0][1]
-            prediction['U'] = result_dru[0][2]
+        # LAYER 2
+        if self.current_symbol == 'D' or self.current_symbol == 'R' or self.current_symbol == 'U':
+            prediction = {'D': result_dru[0][0], 'R': result_dru[0][1], 'U': result_dru[0][2]}
             prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
             self.current_symbol = prediction[0][0]
 
-        # For a group of character, which includes D, I, k,and T.
         if (
                 self.current_symbol == 'D' or self.current_symbol == 'I' or self.current_symbol == 'K' or self.current_symbol == 'T'):
-            prediction = {}
-            prediction['D'] = result_tkdi[0][0]
-            prediction['I'] = result_tkdi[0][1]
-            prediction['K'] = result_tkdi[0][2]
-            prediction['T'] = result_tkdi[0][3]
+            prediction = {'D': result_tkdi[0][0], 'I': result_tkdi[0][1], 'K': result_tkdi[0][2],
+                          'T': result_tkdi[0][3]}
             prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
             self.current_symbol = prediction[0][0]
 
-        # For a group of character, which includes M, N, and S.
         if self.current_symbol == 'M' or self.current_symbol == 'N' or self.current_symbol == 'S':
-            prediction1 = {}
-            prediction1['M'] = result_smn[0][0]
-            prediction1['N'] = result_smn[0][1]
-            prediction1['S'] = result_smn[0][2]
+            prediction1 = {'M': result_smn[0][0], 'N': result_smn[0][1], 'S': result_smn[0][2]}
             prediction1 = sorted(prediction1.items(), key=operator.itemgetter(1), reverse=True)
             if prediction1[0][0] == 'S':
                 self.current_symbol = prediction1[0][0]
             else:
                 self.current_symbol = prediction[0][0]
 
-        # If there is nothing in the screen, then it is "Blank"
-        if self.current_symbol == 'Blank':
+        if self.current_symbol == 'blank':
             for i in ascii_uppercase:
                 self.ct[i] = 0
         self.ct[self.current_symbol] += 1
@@ -213,14 +227,14 @@ class Application:
                 if tmp < 0:
                     tmp *= -1
                 if tmp <= 20:
-                    self.ct['Blank'] = 0
+                    self.ct['blank'] = 0
                     for i in ascii_uppercase:
                         self.ct[i] = 0
                     return
-            self.ct['Blank'] = 0
+            self.ct['blank'] = 0
             for i in ascii_uppercase:
                 self.ct[i] = 0
-            if self.current_symbol == 'Blank':
+            if self.current_symbol == 'blank':
                 if self.blank_flag == 0:
                     self.blank_flag = 1
                     if len(self.str) > 0:
@@ -233,12 +247,14 @@ class Application:
                 self.blank_flag = 0
                 self.word += self.current_symbol
 
+        print(self.current_symbol, self.word)
+
         self.engine.say(self.current_symbol)
         self.engine.runAndWait()
 
     # Destroy the window
     def destructor(self):
-        print("Terminate the Application...")
+        print("Closing Application...")
         self.root.destroy()
         self.vs.release()
         cv2.destroyAllWindows()
@@ -246,6 +262,6 @@ class Application:
 
 # Execution of the program starts here
 
-print("Starting the Application...")
+print("Starting Application...")
 pba = Application()
 pba.root.mainloop()
